646b013b6595412077e19c2d19ca8c387b23b7fc
YiTingChenJeffrey <jeffreychen666@gmail.com>
2017-06-04 20:46:04 +0800

    1

---
+++ app.js
@@ -0,0 +1,151 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Blipp = require('blipp');
+const Path = require('path');
+const Inert = require('inert');
+const Vision = require('vision');
+const Handlebars = require('handlebars');
+
+const server = new Hapi.Server({
+
+    connections:{
+
+        routes:{
+            files:{
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    }
+
+});
+
+server.connection({ port: 3000, host: 'localhost' });
+
+server.register([Blipp, Inert, Vision], ()=> {});
+
+server.views({
+    engines: {
+        html: Handlebars
+    },
+    path: 'views',
+    layoutPath: 'views/layout',
+    layout: 'layout',
+    helpersPath: 'views/helpers'
+
+
+});
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler:{
+        directory:{
+            path: './',
+            listing: true,
+            index: false,
+            redirectToSlash: true
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: {
+        view: {
+            template: 'index'
+        }
+    }
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/specialolympics',
+    handler: {
+        view: {
+            template: 'specialolympics'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/datavisualization',
+    handler: {
+        view: {
+            template: 'datavisualization'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/3dcharacter',
+    handler: {
+        view: {
+            template: '3dcharacter'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/resume',
+    handler: {
+        view: {
+            template: 'resume'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/2dcharacter',
+    handler: {
+        view: {
+            template: '2dcharacter'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/treasurekey',
+    handler: {
+        view: {
+            template: 'treasurekey'
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/specialeffects',
+    handler: {
+        view: {
+            template: 'specialeffects'
+        }
+    }
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/portfolio',
+    handler: {
+        view: {
+            template: 'portfolio'
+        }
+    }
+});
+
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});
